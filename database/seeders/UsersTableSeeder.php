bc2c9f4916d68a5754a0471c4125c7ad1b3771dc
DESKTOP-ODT9EHO\chage <teni410401s@gmail.com>
2025-06-19 23:44:18 +0900

    改修内容： ■シードを行いusersテーブルとsubjectsテーブルに初期データを登録する ■SubjectsTableSeederには「国語、数学、英語」のレコードを作成する

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,5 +16,18 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 
+        DB::table('users')->insert([
+            [
+                'over_name' => '佐藤',
+                'under_name' => '優子',
+                'over_name_kana' => 'サトウ',
+                'under_name_kana' => 'ユウコ',
+                'mail_address' => 'yukosato@gmail.com',
+                'sex' => '2',
+                'birth_day' => '2000-04-04',
+                'role' => '4',
+                'password' => bcrypt('password')
+            ]
+        ]);
     }
 }
